cdd18e618626a92516d1bb43dc0bf020a898fd04
Ruel Angelo P. Sinday <21-1-02429@vsu.edu.ph>
2025-10-16 07:39:23 +0800

    Refactor and update layout.tsx

--- tip-calc-sinday/src/app/layout.tsx
+++ tip-calc-sinday/src/app/layout.tsx
@@ -1,15 +1,26 @@
 import "./globals.css";
 import { Metadata } from "next";
+import {Space_Mono} from "next/font/google";
+
+const spaceMono = Space_Mono({
+  weight: '700',
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export const metadata: Metadata = {
-  title: "tip-calc-sinday",
-  description: "A tip calculator app built with Next.js and Tailwind CSS",
+  title: 'Tip Calculator App',
+  description: 'Split your bill and calculate tips easily',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode;}) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body>      
+      <body className={spaceMono.className}>
         {children}
       </body>
     </html>
